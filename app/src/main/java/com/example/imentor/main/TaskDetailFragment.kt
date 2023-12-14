f7b5d57fa9b6207ec8cad2374d364d89766be21c
Berke Erçetin <82932880+berkeercetin@users.noreply.github.com>
2023-12-14 17:21:43 +0300

    ok

--- app/src/main/java/com/example/imentor/main/TaskDetailFragment.kt
+++ app/src/main/java/com/example/imentor/main/TaskDetailFragment.kt
@@ -7,68 +7,76 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.imentor.R
 import com.example.imentor.main.adapters.TaskAdapter
+import com.example.imentor.main.entities.SubTask
 import com.example.imentor.main.entities.Task
 import com.example.imentor.main.services.concrates.TaskManager
+import com.google.android.material.textview.MaterialTextView
+import com.google.firebase.firestore.QuerySnapshot
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 
 class TaskDetailFragment : Fragment() {
 
     private val taskService = TaskManager()
+    private lateinit var subTasks: List<SubTask>
+    private lateinit var task: Task
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_task_detail, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerViewDetail)!!
-        val taskDetailName = view.findViewById<com.google.android.material.textview.MaterialTextView>(R.id.taskDetailName)!!
-        val taskDetalExplantation = view.findViewById<com.google.android.material.textview.MaterialTextView>(R.id.taskDetailExplanation)!!
-        val taskDetailCounter = view.findViewById<com.google.android.material.textview.MaterialTextView>(R.id.taskDetailCounter)!!
-        val data = arguments?.getString("taskID")
-        try {
-            taskService.getTaskById(GlobalService.userId, data!!).addOnSuccessListener {
-                    result ->
-                taskDetailName.text = result.data?.get("name").toString()
-                taskDetalExplantation.text = result.data?.get("explantation").toString()
-                //taskDetailCounter.text = result.data?.get("counter").toString()
+        val taskDetailName = view.findViewById<MaterialTextView>(R.id.taskDetailName)!!
+        val taskDetailExplanation = view.findViewById<MaterialTextView>(R.id.taskDetailExplanation)!!
+        arguments?.let {
+            val taskId = it.getString("taskID")!!
+            lifecycleScope.launch {
+                try {
+                    task = getTask(taskId)
+                    subTasks = listSubTasks(GlobalService.userId, taskId)
+                    Toast.makeText(context, "Both initialized", Toast.LENGTH_LONG).show()
+                    taskDetailName.text = task.name
+                    taskDetailExplanation.text = task.explantation
+                    // Both tasks are completed
+                } catch (e: Exception) {
+                    // Handle exceptions appropriately
+                    Toast.makeText(context, "Error: ${e.message}", Toast.LENGTH_LONG).show()
+                }
             }
-        } catch (e: Exception) {
-            Toast.makeText(context, "Detail: ${e.message}", Toast.LENGTH_LONG).show()
-        }
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        arguments?.getString("taskID")?.let {
-            taskService.listSubTasks(GlobalService.userId, it).addOnSuccessListener { result ->
-                for (document in result) {
-                    Log.d("TAG", document.data.toString())
-                }
-                val taskList = result.documents.map { it -> it.data }
-                if (taskList.isNotEmpty()) {
-                    // Veriler mevcut, RecyclerView.Adapter'e aktarın
-                    val adapter = TaskAdapter(taskList,requireContext())
-                    recyclerView.adapter = adapter
-                    adapter.notifyDataSetChanged()
-
-                }
-                Log.e("TAG",taskList.size.toString())
-            }
-                .addOnFailureListener { exception ->
-                    Log.d("TAG", "Error getting documents: ", exception)
-                }
         }
         super.onViewCreated(view, savedInstanceState)
     }
-}
+
+    private suspend fun listSubTasks(userId: String, taskId: String): List<SubTask> {
+        val result = taskService.listSubTasks(userId, taskId).await()
+        return result.map {
+            SubTask(it["name"] as String, it["subTaskID"] as String)
+        }
+    }
+
+    private suspend fun getTask(taskId: String): Task {
+        val result = taskService.getTaskById(GlobalService.userId, taskId).await()
+        return result.toObject(Task::class.java)!!
+    }
+
+    }
--- app/src/main/java/com/example/imentor/main/entities/SubTask.kt
+++ app/src/main/java/com/example/imentor/main/entities/SubTask.kt
@@ -4,6 +4,6 @@ package com.example.imentor.main.entities
 data class SubTask(
     val name: String? = null,
     var subTaskID: String? = null,
-    var complated: Boolean? = null,
+    //var complated: Boolean? = null,
 
     )
